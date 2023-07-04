fd0de7366c7fb48a8d715cb9103f4c16a9cc0dbc
Mantou <535979884@qq.com>
2023-07-04 21:03:13 +0800

    feat-登录页增加验证码登录

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,30 +1,43 @@
 <template>
   <div class="login-page">
     <div class="lp-box">
-      <div class="lp-box-left">111</div>
+      <div class="lp-box-left">
+        <!-- TODO -->
+      </div>
       <div class="lp-box-content">
         <div class="info-box">
           <div class="title">中后台管理系统</div>
           <div class="sub-title">Welcome</div>
 
           <el-form class="login-form" label-position="top" label-width="100px" :model="loginForm">
-            <el-form-item label="用户名">
-              <el-input v-model="loginForm.username" />
+            <el-form-item label="账号">
+              <el-input v-model="loginForm.account" />
             </el-form-item>
-            <el-form-item label="密码">
+            <el-form-item v-show="loginType === 1" label="密码">
               <el-input v-model="loginForm.password" type="password" show-password />
             </el-form-item>
+            <el-form-item v-show="loginType === 2" label="手机验证码">
+              <el-input v-model="loginForm.code">
+                <template #append>
+                  <el-button v-if="countdown === 60" class="link" link @click="onStartTimer"
+                    >获取验证码</el-button
+                  >
+                  <span v-else class="countdown">还剩{{ countdown }}秒</span>
+                </template>
+              </el-input>
+            </el-form-item>
             <div class="tip">
-              <el-button class="link" link size="default">忘记密码</el-button>
+              <el-button class="link" link @click="onMockLogin">体验账号登录</el-button>
+              <el-button class="link" link @click="onForget">忘记密码</el-button>
             </div>
           </el-form>
 
           <div class="btn">
-            <el-button class="login" type="primary" size="default" round @click="onLogin"
-              >登 录</el-button
-            >
+            <el-button class="login" type="primary" round @click="onLogin">登 录</el-button>
             <el-divider content-position="center">or</el-divider>
-            <el-button class="link" link size="default">用手机验证码登录</el-button>
+            <el-button class="link" link @click="loginType = loginType === 1 ? 2 : 1">{{
+              loginType === 1 ? '用手机验证码登录' : '用账号密码登录'
+            }}</el-button>
           </div>
         </div>
       </div>
@@ -34,15 +47,40 @@
 
 <script setup lang="ts">
 interface UserInfo {
-  username: string
-  password: string
+  account: string
+  password?: string
+  code?: string
 }
 
 const router = useRouter()
 const loginForm: UserInfo = reactive({
-  username: '',
-  password: ''
+  account: '',
+  password: '',
+  code: ''
 })
+// 登录模式
+const loginType = ref<number>(1)
+// 验证码
+let timer: number
+const countdown = ref<number>(60)
+
+function onStartTimer() {
+  timer = setInterval(() => {
+    countdown.value--
+    if (countdown.value === 0) {
+      clearInterval(timer)
+      timer = 0
+      countdown.value = 60
+    }
+  }, 1000)
+}
+
+function onMockLogin() {
+  loginForm.account = 'admin'
+  loginForm.password = '123456'
+}
+
+function onForget() {}
 
 function onLogin() {
   router.push({
@@ -67,9 +105,12 @@ function onLogin() {
   width: 1056px;
   height: 70vh;
   border-radius: 16px;
+  box-shadow: 10px 10px 10px $colors-shadow;
   .lp-box-left {
     width: 55%;
     background-color: $colors-blue;
+    border-top-left-radius: 16px;
+    border-bottom-left-radius: 16px;
   }
   .lp-box-content {
     width: 45%;
@@ -88,14 +129,28 @@ function onLogin() {
     font-weight: bold;
   }
   .login-form {
-    ::v-deep .el-input__wrapper {
+    :deep(.el-input__wrapper) {
       box-shadow: none;
       border-bottom: var(--el-border-color) 1px solid;
       padding: 0;
     }
+    :deep(.el-input-group__append) {
+      background: none;
+      border: none;
+      box-shadow: none;
+      border-bottom: var(--el-border-color) 1px solid;
+      // color: inherit;
+      --el-color-info: var(--el-color-primary);
+    }
+    .countdown {
+      color: $colors-disabled;
+      cursor: not-allowed;
+    }
   }
   .tip {
-    text-align: right;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
   .login {
     width: 50%;
@@ -105,7 +160,7 @@ function onLogin() {
     --el-button-hover-link-text-color: var(--el-color-primary-light-3);
     --el-button-text-color: var(--el-color-primary);
   }
-  ::v-deep .el-divider .el-divider__text {
+  :deep(.el-divider .el-divider__text) {
     color: var(--el-border-color);
   }
 }
